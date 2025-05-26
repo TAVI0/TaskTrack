4b2f3ab2e0ec0f7babfe3011d8ad57752b6b37e0
TAVI0 <marcos_tavio@outlook.com>
2025-05-26 16:56:56 -0300

    modificaciones para test

--- src/test/java/com/tavio/lemon/LemonApplicationTests.java
+++ src/test/java/com/tavio/lemon/LemonApplicationTests.java
@@ -2,8 +2,10 @@ package com.tavio.lemon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LemonApplicationTests {
 
 	@Test
