94cedcb5ed0f28035f9616f7b9a2f66afc011f0d
tavio <marcos_tavio@outlook.com>
2026-01-02 05:51:32 -0300

    cambio de security para AWS

--- src/main/java/com/tavio/lemon/web/config/SecurityConfig.java
+++ src/main/java/com/tavio/lemon/web/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/auth/register", "/auth/login").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
